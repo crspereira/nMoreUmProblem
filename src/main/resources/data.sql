a5b5556268ec41523e4e25b219989332598ec1e8
Clayton Pereira <clayton.pereira@outlook.com.br>
2021-02-07 17:18:31 -0300

    Domain Model and ORM with Seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO tb_product (name) VALUES ('P1'), ('P2'), ('P3'), ('P4'), ('P5'),
+									 ('P6'), ('P7'), ('P8'), ('P9'), ('P10');
+
+INSERT INTO tb_category (name) VALUES ('Mobiles'), ('Computers'), ('Eletronics');
+
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1 , 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1 , 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2 , 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2 , 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3 , 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3 , 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4 , 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4 , 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (5 , 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (5 , 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (6 , 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (6 , 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (7 , 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (8 , 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (9 , 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (9 , 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (10 , 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (10 , 3);
